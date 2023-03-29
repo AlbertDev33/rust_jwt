524060bd07e6edf5a0ce74639ff0031931a27193
Albert <albert.rocha@r2t.company>
2023-03-29 20:39:02 -0300

    feature: created helth checker route

---
+++ src/handlers/helth_checker.rs
@@ -0,0 +1,9 @@
+use actix_web::{get, HttpResponse, Responder};
+use serde_json;
+
+#[get("/api/healthchecker")]
+async fn health_checker_handler() -> impl Responder {
+    const MESSAGE: &str = "JWT Authentication in Rust using Actix-web, Postgres and SQLX";
+    let response = HttpResponse::Ok().json(serde_json::json!({ "status": "success", "message": MESSAGE }));
+    return response;
+}
